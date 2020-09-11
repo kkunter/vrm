119a233bcb814382ea963d3e06cf8a4624ec2907
Künter Pärtel <kunterpartel@gmail.com>
2020-09-11 07:22:13 +0300

    Thymeleaf working
    Thymeleaf working

--- src/main/java/com/example/projectmanangment/repository/EmployeeRepository.java
+++ src/main/java/com/example/projectmanangment/repository/EmployeeRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     @Query(nativeQuery=true, value="SELECT e.first_name as firstName, e.last_name as lastName, COUNT(pe.employee_id) as projectCount " +
-            "FROM employee e left join project_employee pe ON pe.employee_id = e.employee_id " +
+            "FROM employees e left join project_employee pe ON pe.employee_id = e.employee_id " +
             "GROUP BY e.first_name, e.last_name ORDER BY 3 DESC")
     public List<EmployeeProject> employeeProjects();
 
